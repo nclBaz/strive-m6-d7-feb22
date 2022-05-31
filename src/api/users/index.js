c113d5816d6beaa159c0c31a9f1b67c5a5798408
Riccardo Gulin <riccardo@nucleode.com>
2022-05-31 11:24:34 +0200

    Adds GET, PUT DELETE

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import createError from "http-errors"
 import UsersModel from "./model.js"
 
 const usersRouter = express.Router()
@@ -15,27 +16,61 @@ usersRouter.post("/", async (req, res, next) => {
 
 usersRouter.get("/", async (req, res, next) => {
   try {
+    const users = await UsersModel.find()
+    res.send(users)
   } catch (error) {
     next(error)
   }
 })
 
-usersRouter.get("/:id", async (req, res, next) => {
+usersRouter.get("/:userId", async (req, res, next) => {
   try {
+    const user = await UsersModel.findById(req.params.userId)
+
+    if (user) {
+      res.send(user)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
 })
 
-usersRouter.put("/:id", async (req, res, next) => {
+usersRouter.put("/:userId", async (req, res, next) => {
   try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId, // WHO
+      req.body, // HOW
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get back the newly updated record you should use the option: new true
+      // By default validation is off here --> runValidators: true
+    )
+
+    // ***************************************** ALTERNATIVE METHOD **********************************************
+    // const user = await UsersModel.findById(req.params.userId) // you get back a MONGOOSE DOCUMENT which is NOT a normal object
+
+    // user.firstName = "John"
+
+    // await user.save()
+
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
 })
 
-usersRouter.delete("/:id", async (req, res, next) => {
+usersRouter.delete("/:userId", async (req, res, next) => {
   try {
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import cors from "cors"
 import listEndpoints from "express-list-endpoints"
 import usersRouter from "./api/users/index.js"
+import { badRequestHandler, genericErrorHandler, notFoundHandler } from "./errorHandlers.js"
 
 const server = express()
 const port = process.env.PORT || 3001
@@ -17,6 +18,10 @@ server.use(express.json())
 server.use("/users", usersRouter)
 
 // ***************************************************** ERROR HANDLERS ********************************************
+
+server.use(badRequestHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
 
 mongoose.connect(process.env.MONGO_CONNECTION_URL)
 
